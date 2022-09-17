f919b186a63f75b716fac280a34be653f6ec3d9c
Cemil Dogan <cemil82@gmail.com>
2022-09-17 21:46:55 +0200

    fix

--- server/src/main/java/ch/designbees/lifttime/config/WebConfig.java
+++ server/src/main/java/ch/designbees/lifttime/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5500");
+        registry.addMapping("/**");
     }
 }
